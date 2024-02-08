05c891d68344bd8ddd69bb78c8e0bfaaa07bbb40
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-08 23:02:04 +0530

    Update modelchanges.py
    Find max pad length

--- source/modelchanges.py
+++ source/modelchanges.py
@@ -14,3 +14,14 @@ test = pd.read_csv('data/test.csv')
 # Check the first 5 rows
 print(train.head())
 print(test.head())
+
+
+def find_max_pad_len(file_paths):
+    max_len = 0
+    for file_path in file_paths:
+        audio, sample_rate = librosa.load(file_path, sr=None)
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        if mfccs.shape[1] > max_len:
+            max_len = mfccs.shape[1]
+    return max_len
+
