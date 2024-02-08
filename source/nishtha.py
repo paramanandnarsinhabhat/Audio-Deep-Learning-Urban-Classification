58f7893b390a249d0545bc7bedf9ea045b023f70
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-08 22:22:40 +0530

    adding imports

---
+++ source/nishtha.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import librosa
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, GlobalAveragePooling1D
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import librosa
+import os
+import numpy as np
+
+# Load training and test data
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+#Check the first 5 rows
+print(train.head())
+
+print(test.head())
+
